4b1b3f91797310f31afd88167be48cb30da21f43
Xavier Hanin <xavier.hanin@gmail.com>
2013-02-05 22:55:46 +0100

    refactor: review equality checks

--- restx/src/main/java/restx/common/MorePreconditions.java
+++ restx/src/main/java/restx/common/MorePreconditions.java
@@ -1,6 +1,8 @@
 package restx.common;
 
+import com.google.common.base.Objects;
 import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
 
 /**
  * User: xavierhanin
@@ -14,5 +16,12 @@ public class MorePreconditions {
         } else {
             throw new IllegalStateException(String.format(msg, parameters));
         }
+    }
+
+    public static <T> T checkEquals(String name1, T o1, String name2, T o2) {
+        Preconditions.checkArgument(Objects.equal(o1, o2),
+                "%s %s must be equal to %s %s", name1, o1, name2, o2);
+
+        return o1;
     }
 }
