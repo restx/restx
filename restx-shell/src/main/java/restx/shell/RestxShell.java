e21884a3aa8ca8b6652f351659305e63cd432ea4
Xavier Hanin <xavier.hanin@gmail.com>
2013-04-18 09:10:06 +0200

    refactor restx shell, making it cleaner and more extendable

--- restx-shell/src/main/java/restx/shell/RestxShell.java
+++ restx-shell/src/main/java/restx/shell/RestxShell.java
@@ -2,12 +2,14 @@ package restx.shell;
 
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 import jline.console.ConsoleReader;
 import jline.console.completer.Completer;
 import restx.factory.Factory;
 import restx.shell.commands.HelpCommand;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Set;
 
@@ -19,68 +21,119 @@ import java.util.Set;
 public class RestxShell {
     public static final class ExitShell extends RuntimeException { }
 
-    public static void main(String[] args) throws Exception {
-        ConsoleReader consoleReader = new ConsoleReader();
+    private final ConsoleReader consoleReader;
+    private final Factory factory;
+    private final ImmutableSet<ShellCommand> commands;
+
+    public RestxShell(ConsoleReader consoleReader) {
+        this(consoleReader, Factory.builder().addFromServiceLoader().build());
+    }
+
+    public RestxShell(ConsoleReader consoleReader, Factory factory) {
+        this.consoleReader = consoleReader;
+        this.factory = factory;
+
+        this.commands = ImmutableSet.copyOf(findCommands());
+
+        initConsole(consoleReader);
+    }
+
+    public ConsoleReader getConsoleReader() {
+        return consoleReader;
+    }
+
+    public Factory getFactory() {
+        return factory;
+    }
+
+    public ImmutableSet<ShellCommand> getCommands() {
+        return commands;
+    }
+
+    public void start() throws IOException {
+        banner();
+
+        installCompleters();
+
+        boolean exit = false;
+        while (!exit) {
+            String line = consoleReader.readLine();
+            exit = exec(line);
+        }
+
+        consoleReader.println("Bye.");
+        consoleReader.shutdown();
+    }
+
+    protected void initConsole(ConsoleReader consoleReader) {
         consoleReader.setPrompt("restx> ");
         consoleReader.setHistoryEnabled(true);
+    }
+
+    protected void banner() throws IOException {
         consoleReader.println("===============================================================================");
         consoleReader.println("== WELCOME TO RESTX SHELL - type `help` for help on available commands");
         consoleReader.println("===============================================================================");
+    }
 
-        Factory factory = Factory.builder().addFromServiceLoader().build();
+    protected boolean exec(String line) throws IOException {
+        boolean found = false;
+        for (ShellCommand command : commands) {
+            Optional<? extends ShellCommandRunner> match = command.match(line);
+            if (match.isPresent()) {
+                String storedPrompt = consoleReader.getPrompt();
+                // store current completers and clean them, so that executing command can perform in a clean env
+                Collection<Completer> storedCompleters = ImmutableList.copyOf(consoleReader.getCompleters());
+                for (Completer completer : storedCompleters) {
+                    consoleReader.removeCompleter(completer);
+                }
 
-        Set<ShellCommand> commands = factory.queryByClass(ShellCommand.class).findAsComponents();
-        HelpCommand helpCommand = new HelpCommand(commands);
-        commands = Sets.newLinkedHashSet(commands);
-        commands.add(helpCommand);
+                try {
+                    match.get().run(consoleReader);
+                    found = true;
+                } catch (ExitShell e) {
+                    return true;
+                } catch (Exception e) {
+                    consoleReader.println("command " + line + " raised an exception: " + e.getMessage());
+                    e.printStackTrace();
+                } finally {
+                    for (Completer completer : ImmutableList.copyOf(consoleReader.getCompleters())) {
+                        consoleReader.removeCompleter(completer);
+                    }
+                    for (Completer completer : storedCompleters) {
+                        consoleReader.addCompleter(completer);
+                    }
+                    consoleReader.setPrompt(storedPrompt);
+                }
+                break;
+            }
+        }
+        if (!found) {
+            consoleReader.println("command not found. use `help` to get the list of commands.");
+        }
+        return false;
+    }
 
+    protected void installCompleters() {
         for (ShellCommand command : commands) {
             for (Completer completer : command.getCompleters()) {
                 consoleReader.addCompleter(completer);
             }
         }
+    }
 
-        boolean exit = false;
-        while (!exit) {
-            String line = consoleReader.readLine();
-            boolean found = false;
-            for (ShellCommand command : commands) {
-                Optional<? extends ShellCommandRunner> match = command.match(line);
-                if (match.isPresent()) {
-                    String storedPrompt = consoleReader.getPrompt();
-                    // store current completers and clean them, so that executing command can perform in a clean env
-                    Collection<Completer> storedCompleters = ImmutableList.copyOf(consoleReader.getCompleters());
-                    for (Completer completer : storedCompleters) {
-                        consoleReader.removeCompleter(completer);
-                    }
+    protected Set<ShellCommand> findCommands() {
+        Set<ShellCommand> commands = factory.queryByClass(ShellCommand.class).findAsComponents();
+        HelpCommand helpCommand = new HelpCommand(commands);
+        commands = Sets.newLinkedHashSet(commands);
+        commands.add(helpCommand);
+        return commands;
+    }
 
-                    try {
-                        match.get().run(consoleReader);
-                        found = true;
-                    } catch (ExitShell e) {
-                        exit = true;
-                    } catch (Exception e) {
-                        consoleReader.println("command " + line + " raised an exception: " + e.getMessage());
-                        e.printStackTrace();
-                    } finally {
-                        for (Completer completer : ImmutableList.copyOf(consoleReader.getCompleters())) {
-                            consoleReader.removeCompleter(completer);
-                        }
-                        for (Completer completer : storedCompleters) {
-                            consoleReader.addCompleter(completer);
-                        }
-                        consoleReader.setPrompt(storedPrompt);
-                    }
-                    break;
-                }
-            }
-            if (!found) {
-                consoleReader.println("command not found. use `help` to get the list of commands.");
-            }
-        }
 
-        consoleReader.println("Bye.");
-        consoleReader.shutdown();
+    public static void main(String[] args) throws Exception {
+        ConsoleReader consoleReader = new ConsoleReader();
+        new RestxShell(consoleReader).start();
     }
 
 }
