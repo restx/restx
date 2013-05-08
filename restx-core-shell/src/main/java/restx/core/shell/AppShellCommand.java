71713478c48c1f7096a34e30a3c49b4eafffc680
Xavier Hanin <xavier.hanin@gmail.com>
2013-05-09 00:17:53 +0200

    improve app bootstrap command: - add help, - generate ivy and/or pom, - use restx version retrieved from current version

--- restx-core-shell/src/main/java/restx/core/shell/AppShellCommand.java
+++ restx-core-shell/src/main/java/restx/core/shell/AppShellCommand.java
@@ -3,13 +3,16 @@ package restx.core.shell;
 import com.github.mustachejava.Mustache;
 import com.google.common.base.Joiner;
 import com.google.common.base.Optional;
+import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import jline.console.completer.ArgumentCompleter;
 import jline.console.completer.Completer;
 import jline.console.completer.StringsCompleter;
+import restx.build.RestxBuild;
 import restx.common.UUIDGenerator;
+import restx.common.Version;
 import restx.factory.Component;
 import restx.shell.RestxShell;
 import restx.shell.ShellCommandRunner;
@@ -57,7 +60,7 @@ public class AppShellCommand extends StdShellCommand {
     private class NewAppCommandRunner implements ShellCommandRunner {
 
         private ImmutableMap<Mustache, String> mainTemplates = buildTemplates(
-                AppShellCommand.class, ImmutableMap.<String,String>builder()
+                AppShellCommand.class, ImmutableMap.<String, String>builder()
                 .put("md.restx.json.mustache", "md.restx.json")
                 .put("AppModule.java.mustache", "src/main/java/{{packagePath}}/AppModule.java")
                 .put("AppServer.java.mustache", "src/main/java/{{packagePath}}/AppServer.java")
@@ -66,7 +69,7 @@ public class AppShellCommand extends StdShellCommand {
                 .build()
         );
         private ImmutableMap<Mustache, String> helloResourceTemplates = buildTemplates(
-                AppShellCommand.class, ImmutableMap.<String,String>builder()
+                AppShellCommand.class, ImmutableMap.<String, String>builder()
                 .put("Message.java.mustache", "src/main/java/{{packagePath}}/domain/Message.java")
                 .put("HelloResource.java.mustache", "src/main/java/{{packagePath}}/rest/HelloResource.java")
                 .put("HelloResourceSpecTest.java.mustache", "src/test/java/{{packagePath}}/rest/HelloResourceSpecTest.java")
@@ -76,26 +79,78 @@ public class AppShellCommand extends StdShellCommand {
 
         @Override
         public void run(RestxShell shell) throws Exception {
-            String appName = shell.getConsoleReader().readLine("App name? ");
+            shell.printIn("Welcome to RESTX APP bootstrap!", RestxShell.AnsiCodes.ANSI_GREEN);
+            shell.println("");
+            shell.println("This command will ask you a few questions to generate your brand new RESTX app.");
+            shell.println("For any question you can get help by answering '??' (without the quotes).");
+            shell.println("");
+
+            String appName = "";
+            while (Strings.isNullOrEmpty(appName)) {
+                appName = shell.ask("App name? ", "",
+                        "This is the name of the application you are creating.\n" +
+                                "It can contain spaces, it's used mainly for documentation and to provide default for other values.\n" +
+                                "Examples: Todo, Foo Bar, ...");
+            }
             String groupId = shell.ask("group id [%s]? ",
-                    appName.replaceAll("\\s+", "-").toLowerCase(Locale.ENGLISH));
+                    appName.replaceAll("\\s+", "-").toLowerCase(Locale.ENGLISH),
+                    "This is the identifier of the group or organization producing the application.\n" +
+                            "In the Maven world this is called a groupId, in Ivy it's called organization.\n" +
+                            "It MUST NOT contain spaces nor columns (':'), and is usually a reversed domain name.\n" +
+                            "Examples: io.restx, com.example, ...");
             String artifactId = shell.ask("artifact id [%s]? ",
-                                        appName.replaceAll("\\s+", "-").toLowerCase(Locale.ENGLISH))
+                    appName.replaceAll("\\s+", "-").toLowerCase(Locale.ENGLISH),
+                    "This is the identifier of the app module.\n" +
+                            "In the Maven world this is called an artifactId, in Ivy it's called module.\n" +
+                            "It MUST NOT contain spaces nor columns (':'), and is usually a dash separated lower case word.\n" +
+                            "Examples: myapp, todo, foo-app, ...")
                     .replaceAll("\\s+", "-");
             String mainPackage = shell.ask("main package [%s]? ",
-                    artifactId.replaceAll("\\-", ".").toLowerCase(Locale.ENGLISH));
-            String version = shell.ask("version [%s]? ", "0.1-SNAPSHOT");
+                    artifactId.replaceAll("\\-", ".").toLowerCase(Locale.ENGLISH),
+                    "This is the main package in which you will develop your application.\n" +
+                            "In Java convention it should start with a reversed domain name followed by the app name\n" +
+                            "but for applications (as opposed to APIs) we prefer to use a short name, like that app name.\n" +
+                            "It MUST follow Java package names restrictions, so MUST NOT contain spaces\n" +
+                            "Examples: myapp, com.example.todoapp, ...");
+            String version = shell.ask("version [%s]? ", "0.1-SNAPSHOT",
+                    "This is the name of the first version of the app you are targetting.\n" +
+                            "It's recommended to use Maven convention to suffix it with -SNAPSHOT if you plan to use Maven for your app\n" +
+                            "Examples: 0.1-SNAPSHOT, 1.0, ...");
+
+            boolean generateIvy = shell.askBoolean("generate ivy file [%s]? ", "yes",
+                    "Answer yes to get an Easyant compatible Ivy file generated for you.\n" +
+                            "If you don't know what dependency management is, answer yes, RESTX will help you with that matter.\n" +
+                            "If you know what you want, note that RESTX can also generate a Maven POM for you.");
+            boolean generatePom = shell.askBoolean("generate maven pom [%s]? ", generateIvy ? "no" : "yes",
+                    "Answer yes to get a Maven POM generated for you.\n" +
+                            "If you don't know what dependency management, use default answer.\n" +
+                            "If you know what you want, you probably shouldn't be reading this help message :).");
+
+            String restxVersion = shell.ask("restx version [%s]? ", Version.getVersion("io.restx", "restx-core"));
 
             List<String> list = Lists.newArrayList(UUIDGenerator.generate(),
                     String.valueOf(new Random().nextLong()), appName, artifactId);
             Collections.shuffle(list);
             String signatureKey = shell.ask("signature key (to sign cookies) [%s]? ",
-                    Joiner.on(" ").join(list));
+                    Joiner.on(" ").join(list),
+                    "This is used as salt for signing stuff exchanged with the client.\n" +
+                            "Use something fancy or keep what is proposed by default, but make sure to not share that publicly.");
 
-            String defaultPort = shell.ask("default port [%s]? ", "8080");
-            String basePath = shell.ask("base path [%s]? ", "/api");
-
-            String restxVersion = "0.2-SNAPSHOT";
+            String defaultPort = shell.ask("default port [%s]? ", "8080",
+                    "This is the default port used when using embedded version.\n" +
+                            "Usually Java web containers use 8080, it may be a good idea to use a different port to avoid \n" +
+                            "conflicts with another servlet container.\n" +
+                            "You can also use port 80 if you want to serve your API directly with the embedded server\n" +
+                            "and no reverse proxy in front of it. But beware that you may need admin privileges for that.\n" +
+                            "Examples: 8080, 8086, 8000, 80");
+            String basePath = shell.ask("base path [%s]? ", "/api",
+                    "This is the base API path on which RESTX will handle requests.\n" +
+                            "Being focused on REST API only, RESTX is usually shared with either static or dynamic \n" +
+                            "resources serving (HTML, CSS, JS, images, ...) and therefore is used to handle requests on\n" +
+                            "only a sub path of the web app.\n" +
+                            "If you plan to use it to serve requests from an API only domain (eg api.example.com)\n" +
+                            "you can use '' (empty string) for this path.\n" +
+                            "Examples: /api, /api/v2, /restx, ...");
 
             ImmutableMap scope = ImmutableMap.builder()
                     .put("appName", appName)
@@ -110,16 +165,33 @@ public class AppShellCommand extends StdShellCommand {
                     .put("restxVersion", restxVersion)
                     .build();
 
-            boolean generateHelloResource = shell.askBoolean("generate hello resource example [Y/n]? ", "y");
+            boolean generateHelloResource = shell.askBoolean("generate hello resource example [Y/n]? ", "y",
+                    "This will generate an example resource with an associated spec test so that your boostrapped\n" +
+                            "application can be used as soon as it has been generated.\n" +
+                            "If this is the first app you generate with RESTX, it's probably a good idea to generate\n" +
+                            "this example resource.\n" +
+                            "If you already know RESTX by heart you shouldn't be reading this message anyway :)");
 
             Path appPath = shell.currentLocation().resolve(artifactId);
 
             shell.println("scaffolding app to `" + appPath.toAbsolutePath() + "` ...");
             generate(mainTemplates, appPath, scope);
 
+            if (generateIvy) {
+                shell.println("generating module.ivy ...");
+                RestxBuild.convert(appPath.toAbsolutePath() + "/md.restx.json", appPath.toAbsolutePath() + "/module.ivy");
+            }
+            if (generatePom) {
+                shell.println("generating pom.xml ...");
+                RestxBuild.convert(appPath.toAbsolutePath() + "/md.restx.json", appPath.toAbsolutePath() + "/pom.xml");
+            }
+
             if (generateHelloResource) {
+                shell.println("generating hello resource ...");
                 generate(helloResourceTemplates, appPath, scope);
             }
+            shell.printIn("Congratulations! - Your app is now ready in " + appPath.toAbsolutePath(), RestxShell.AnsiCodes.ANSI_GREEN);
+            shell.println("");
         }
     }
 }
