96e0356dc830bd1bada98dd94f0c764497c41ae1
Xavier Hanin <xavier.hanin@gmail.com>
2013-05-10 00:24:46 +0200

    add a toc to doc pages and improve style and responsiveness

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-$('.docs img').click(function() { $(this).toggleClass('large') })
+$(function() {
+	$('.docs img').click(function() { $(this).toggleClass('large') });
+	
+	if ($('.docs').size() > 0) {
+		$('#toc').show().toc();
+		$('.inner').addClass('docs');
+	} 
+})
