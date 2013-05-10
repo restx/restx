66d0079e07933ebcc8226efa57c52a988b0d6f7a
Xavier Hanin <xavier.hanin@gmail.com>
2013-05-11 01:16:18 +0200

    improve home page

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,4 +5,21 @@ $(function() {
 		$('#toc').show().toc();
 		$('.inner').addClass('docs');
 	} 
+	
+	$('.banner').each(function(){
+		var delay = $(this).data('delay') || 3000;
+		var init = $(this).data('init') || 0;
+		var loop = $(this).data('loop') || false;
+		$(this).unslider({delay: delay, init: init, loop: loop, dots: true, keys:false});
+	});
+	
+	$('.unslider-arrow').click(function() {
+      var fn = this.className.split(' ')[1];
+  
+      //  Either do unslider.data('unslider').next() or .prev() depending on the className
+			$(this).closest('.banner').data('unslider')[fn]();
+			
+			return false;
+  });
+	
 })
