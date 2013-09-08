18bdc50ca133b7964557575ac6b04454ebbca7be
Xavier Hanin <xavier.hanin@gmail.com>
2013-09-08 13:43:24 +0200

    api docs: add send & fix feature
    This let one record a request/response and then fix expectation, useful when you try out a request and see that the response is not the expected one.

--- restx-apidocs/src/main/resources/restx/apidocs/js/controllers/OperationController.js
+++ restx-apidocs/src/main/resources/restx/apidocs/js/controllers/OperationController.js
@@ -103,18 +103,59 @@ adminApp.controller('OperationController', function OperationController(
         };
     }
 
+    function prepareRequestForRecord() {
+        $scope.request = prepareRequest();
+        $scope.request.headers['RestxMode'] = 'recording';
+        $scope.request.headers['RestxRecordPath'] = 'specs/' + $routeParams.name;
+        $scope.request.headers['RestxRecordTitle'] = _.str.humanize($scope.operation.nickname) + ' '
+            + $filter('date')(new Date(), 'yyyyMMdd HHmm');
+    }
+
     $scope.send = function() {
         $scope.request = prepareRequest();
         sendRequest();
     }
 
+
     $scope.sendAndRecord = function() {
-        $scope.request = prepareRequest();
-        $scope.request.headers['RestxMode'] = 'recording';
-        $scope.request.headers['RestxRecordPath'] = $routeParams.name;
-        $scope.request.headers['RestxRecordTitle'] = _.str.humanize($scope.operation.nickname) + ' '
-            + $filter('date')(new Date(), 'yyyyMMdd HHmm');
+        prepareRequestForRecord();
         sendRequest(function() { loadAllSpecs(); });
+    }
+
+    $scope.sendAndFix = function() {
+        prepareRequestForRecord();
+        sendRequest(function(data, status, headers) {
+            var spec = headers('RestxSpecPath');
+
+            $scope.expect = {
+                specPath: spec,
+                status: status,
+                body: data
+            };
+        });
+    }
+
+    $scope.fix = function() {
+        var spec = $scope.expect.specPath;
+        $http.put('../../specs/' + encodeURIComponent(spec) + '/wts/0/then',
+            {expectedCode: $scope.expect.status, expected: $scope.expect.body }).
+            success(function () {
+                loadSpec(spec);
+            });
+        delete $scope.expect;
+    }
+
+    function loadSpec(spec) {
+        $http.get('../../specs/' + encodeURIComponent(spec)).
+            success(function (data) {
+                spec = { title: data.title, requests: [] };
+                data.whens.forEach(function (when) {
+                    spec.requests.push({
+                        httpMethod: when.method, path: '/' + when.path, body: when.body, showBody: when.body.trim() !== '',
+                        response: { body: when.then.expected, status: when.then.expectedCode }});
+                });
+                $scope.specs.push(spec);
+            });
     }
 
     function loadAllSpecs() {
@@ -122,16 +163,7 @@ adminApp.controller('OperationController', function OperationController(
         $http.get('../../specs', {params: {httpMethod: $routeParams.httpMethod, path: path}}).
             success(function (data) {
                 data.forEach(function (spec) {
-                    $http.get('../../specs/' + encodeURIComponent(spec)).
-                        success(function (data) {
-                            spec = { title: data.title, requests: [] };
-                            data.whens.forEach(function (when) {
-                                spec.requests.push({
-                                    httpMethod: when.method, path: '/' + when.path, body: when.body, showBody: when.body.trim() !== '',
-                                    response: { body: when.then.expected, status: when.then.expectedCode }});
-                            });
-                            $scope.specs.push(spec);
-                        });
+                    loadSpec(spec);
                 });
             });
     }
