5023e9058fe201e63c953ed817ff12284435f710
xhanin <xavier.hanin@4sh.fr>
2013-11-17 01:00:24 +0100

    monitor-admin: setup jvm metrics and jmx + graphite reporters

---
+++ restx-monitor-admin/src/main/java/restx/monitor/GraphiteSettings.java
@@ -0,0 +1,23 @@
+package restx.monitor;
+
+import com.google.common.base.Optional;
+import restx.config.Settings;
+import restx.config.SettingsKey;
+
+/**
+ * Date: 17/11/13
+ * Time: 00:32
+ */
+@Settings
+public interface GraphiteSettings {
+    @SettingsKey(key = "graphite.host", doc = "the host at which a graphite server is listening to collect metrics info" +
+            " - set it to activate graphite reporting")
+    Optional<String> getGraphiteHost();
+    @SettingsKey(key = "graphite.port", doc = "the port at which a graphite server is listening to collect metrics info")
+    Optional<Integer> getGraphitePort();
+
+    @SettingsKey(key = "graphite.reporter.frequency", doc = "the frequency at which data is sent to graphite", defaultValue = "1")
+    Optional<Integer> getFrequency();
+    @SettingsKey(key = "graphite.reporter.frequency.unit", doc = "the unit in which frequency is expressed", defaultValue = "MINUTES")
+    Optional<String> getFrequencyUnit();
+}
---
+++ restx-monitor-admin/src/main/java/restx/monitor/MetricsConfiguration.java
@@ -0,0 +1,64 @@
+package restx.monitor;
+
+import com.codahale.metrics.JmxReporter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.graphite.Graphite;
+import com.codahale.metrics.graphite.GraphiteReporter;
+import com.codahale.metrics.jvm.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import restx.factory.AutoStartable;
+import restx.factory.Component;
+
+import java.lang.management.ManagementFactory;
+import java.net.InetSocketAddress;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Date: 17/11/13
+ * Time: 00:25
+ */
+@Component
+public class MetricsConfiguration implements AutoStartable {
+    private static final Logger logger = LoggerFactory.getLogger(MetricsConfiguration.class);
+
+    private final MetricRegistry metrics;
+    private final GraphiteSettings graphiteSettings;
+
+    public MetricsConfiguration(MetricRegistry metrics, GraphiteSettings graphiteSettings) {
+        this.metrics = metrics;
+        this.graphiteSettings = graphiteSettings;
+    }
+
+    @Override
+    public void start() {
+        metrics.register("jvm.memory", new MemoryUsageGaugeSet());
+        metrics.register("jvm.garbage", new GarbageCollectorMetricSet());
+        metrics.register("jvm.threads", new ThreadStatesGaugeSet());
+        metrics.register("jvm.files", new FileDescriptorRatioGauge());
+        metrics.register("jvm.buffers", new BufferPoolMetricSet(ManagementFactory.getPlatformMBeanServer()));
+
+        setupReporters();
+    }
+
+    protected void setupReporters() {
+        final JmxReporter jmxReporter = JmxReporter.forRegistry(metrics).build();
+        jmxReporter.start();
+
+        setupGraphiteReporter();
+    }
+
+    private void setupGraphiteReporter() {
+        if (graphiteSettings.getGraphiteHost().isPresent()) {
+            InetSocketAddress address = new InetSocketAddress(
+                    graphiteSettings.getGraphiteHost().get(), graphiteSettings.getGraphitePort().or(2003));
+            logger.info("Initializing Metrics Graphite reporting to {}", address);
+            GraphiteReporter graphiteReporter = GraphiteReporter.forRegistry(metrics)
+                    .convertRatesTo(TimeUnit.SECONDS)
+                    .convertDurationsTo(TimeUnit.MILLISECONDS)
+                    .build(new Graphite(address));
+            graphiteReporter.start(graphiteSettings.getFrequency().get(),
+                    TimeUnit.valueOf(graphiteSettings.getFrequencyUnit().get()));
+        }
+    }
+}
