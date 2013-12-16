8ff55491ce7601921685713010214bba66ba8c0e
xhanin <xavier.hanin@4sh.fr>
2013-12-16 23:25:32 +0100

    samplest: fix failing test due to bad authentication

--- restx-samplest/src/test/java/samplest/core/ProvidesWithExceptionsTest.java
+++ restx-samplest/src/test/java/samplest/core/ProvidesWithExceptionsTest.java
@@ -8,16 +8,16 @@ import restx.tests.RestxServerRule;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * @see ContextParamsResource
+ * @see samplest.core.ProvidesWithExceptionResource
  */
 public class ProvidesWithExceptionsTest {
     @ClassRule
     public static RestxServerRule server = new RestxServerRule();
 
     @Test
-    public void should_access_base_uri() throws Exception {
-        HttpRequest httpRequest = server.client().GET(
-                "/api/providesWithExceptions");
+    public void should_access_resource_relying_on_module_declaring_exceptions() throws Exception {
+        HttpRequest httpRequest = server.client().authenticatedAs("admin")
+                .GET("/api/providesWithExceptions");
         assertThat(httpRequest.code()).isEqualTo(200);
         assertThat(httpRequest.body().trim()).isEqualTo("noproblem");
     }
