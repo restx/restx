76b1e17c7c79d543c326e9718c374ca333f9fbe8
xhanin <xavier.hanin@4sh.fr>
2013-12-27 10:11:10 +0100

    samplest: add samplest for some query params

--- restx-samplest/src/main/java/samplest/core/ParametersResource.java
+++ restx-samplest/src/main/java/samplest/core/ParametersResource.java
@@ -1,5 +1,8 @@
 package samplest.core;
 
+import com.google.common.base.Optional;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import restx.annotations.GET;
 import restx.annotations.RestxResource;
 import restx.factory.Component;
@@ -13,5 +16,15 @@ public class ParametersResource {
     @GET("/params/path/{a}/:b/{c:\\d+}:d")
     public String pathparams(String a, String b, String c, String d) {
         return "a=" + a + " b=" + b + " c=" + c + " d=" + d;
+    }
+
+    @GET("/params/query/1")
+    public String queryparams(String a, Optional<String> b) {
+        return "a=" + a + " b=" + b.or("default");
+    }
+
+    @GET("/params/query/2")
+    public String queryparams2(DateTime a, Optional<DateTime> b) {
+        return "a=" + a + " b=" + b.or(new DateTime(0L, DateTimeZone.UTC));
     }
 }
--- restx-samplest/src/test/java/samplest/core/ParametersResourceTest.java
+++ restx-samplest/src/test/java/samplest/core/ParametersResourceTest.java
@@ -5,6 +5,8 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import restx.tests.RestxServerRule;
 
+import java.net.URLEncoder;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -21,6 +23,41 @@ public class ParametersResourceTest {
                 "/api/params/path/v1/v2/35v4");
         assertThat(httpRequest.code()).isEqualTo(200);
         assertThat(httpRequest.body().trim()).isEqualTo("a=v1 b=v2 c=35 d=v4");
+    }
+
+    @Test
+    public void should_return_query_params() throws Exception {
+        HttpRequest httpRequest = server.client().authenticatedAs("admin").GET(
+                "/api/params/query/1?a=v1&b=v2");
+        assertThat(httpRequest.code()).isEqualTo(200);
+        assertThat(httpRequest.body().trim()).isEqualTo("a=v1 b=v2");
+    }
+
+    @Test
+    public void should_return_query_params_without_optional() throws Exception {
+        HttpRequest httpRequest = server.client().authenticatedAs("admin").GET(
+                "/api/params/query/1?a=v1");
+        assertThat(httpRequest.code()).isEqualTo(200);
+        assertThat(httpRequest.body().trim()).isEqualTo("a=v1 b=default");
+    }
+
+    @Test
+    public void should_return_query_params_datetime() throws Exception {
+        HttpRequest httpRequest = server.client().authenticatedAs("admin").GET(
+                "/api/params/query/2" +
+                        "?a=" + URLEncoder.encode("2013-11-20T14:00:00.000+01:00", "UTF-8") +
+                        "&b=" + URLEncoder.encode("2013-12-20T14:00:00.000+01:00", "UTF-8"));
+        assertThat(httpRequest.code()).isEqualTo(200);
+        assertThat(httpRequest.body().trim()).isEqualTo("a=2013-11-20T13:00:00.000Z b=2013-12-20T13:00:00.000Z");
+    }
+
+    @Test
+    public void should_return_query_params_datetime_without_optional() throws Exception {
+        HttpRequest httpRequest = server.client().authenticatedAs("admin").GET(
+                "/api/params/query/2" +
+                        "?a=" + URLEncoder.encode("2013-11-20T14:00:00.000+01:00", "UTF-8"));
+        assertThat(httpRequest.code()).isEqualTo(200);
+        assertThat(httpRequest.body().trim()).isEqualTo("a=2013-11-20T13:00:00.000Z b=1970-01-01T00:00:00.000Z");
     }
 
 }
