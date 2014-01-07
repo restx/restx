8c6b6fda80aac6a14c6bbdea846da8be6a37bf30
xhanin <xavier.hanin@4sh.fr>
2014-01-07 01:18:24 +0100

    add script to test restx-package more easily

---
+++ restx-package/local-install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+PLUGINS="core-shell
+specs-shell
+build-shell"
+
+# shell script to prepare a local install of restx shell
+# you need to have built restx-package and the plugins first
+
+rm -rf target/restx
+mkdir target/restx
+cd target/restx
+tar xzvf ../assembly/out/restx-0.31-SNAPSHOT.tar.gz
+
+cd -
+
+for plugin in $PLUGINS
+do
+  cd ../restx-$plugin
+  mvn -o dependency:copy-dependencies
+  cp target/dependency/* ../restx-package/target/restx/plugins/
+  cp `ls -1 target/*.jar | grep -v javadoc | grep -v sources`  ../restx-package/target/restx/plugins/
+  cd -
+done
+
+echo "local restx shell ready in target/restx"
+echo "you can run it with:"
+echo "  target/restx/restx"
