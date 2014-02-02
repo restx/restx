d675737325c6b3607b9758b7c28e3b0ef4c53211
xhanin <xavier.hanin@4sh.fr>
2014-02-02 11:26:40 +0100

    core - i18n - add jackson mapping to MessageParams

--- restx-i18n/src/main/java/restx/i18n/MessageParams.java
+++ restx-i18n/src/main/java/restx/i18n/MessageParams.java
@@ -1,13 +1,27 @@
 package restx.i18n;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.type.MapType;
+import com.fasterxml.jackson.databind.type.SimpleType;
 import com.google.common.collect.ImmutableMap;
 
+import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
  * Date: 25/1/14
  * Time: 14:37
  */
+@JsonSerialize(using = MessageParams.MessageParamsSerializer.class)
+@JsonDeserialize(using = MessageParams.MessageParamsDeserializer.class)
 public class MessageParams {
     private static final MessageParams EMPTY = new MessageParams(ImmutableMap.<String, Object>of());
 
@@ -32,5 +46,31 @@ public class MessageParams {
 
     public Map<String, ?> toMap() {
         return map;
+    }
+
+    @Override
+    public String toString() {
+        return "MessageParams{" +
+                "map=" + map +
+                '}';
+    }
+
+    public static class MessageParamsSerializer extends JsonSerializer<MessageParams> {
+        @Override
+        public void serialize(MessageParams value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+            provider.defaultSerializeValue(value.toMap(), jgen);
+        }
+    }
+
+    public static class MessageParamsDeserializer extends JsonDeserializer<MessageParams> {
+        @Override
+        public MessageParams deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+            JsonDeserializer<Object> mapDeser = ctxt.findRootValueDeserializer(MapType.construct(
+                    LinkedHashMap.class,
+                    SimpleType.construct(String.class),
+                    SimpleType.construct(Object.class)));
+            Map<String, Object> m = (Map<String, Object>) mapDeser.deserialize(jp, ctxt);
+            return new MessageParams(ImmutableMap.copyOf(m));
+        }
     }
 }
---
+++ restx-i18n/src/test/java/restx/i18n/MessageParamsTest.java
@@ -0,0 +1,34 @@
+package restx.i18n;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
+
+/**
+ * Date: 2/2/14
+ * Time: 11:07
+ */
+public class MessageParamsTest {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void should_serialize() throws Exception {
+        String s = objectMapper.writer().writeValueAsString(MessageParams.of("key1", "value1"));
+        assertThat(s).isEqualTo("{\"key1\":\"value1\"}");
+    }
+
+    @Test
+    public void should_serialize_deserialize() throws Exception {
+        assertSerializeDeserialize(MessageParams.empty());
+        assertSerializeDeserialize(MessageParams.of("key1", "value1"));
+    }
+
+    protected void assertSerializeDeserialize(MessageParams params) throws java.io.IOException {
+        String s = objectMapper.writer().writeValueAsString(params);
+        MessageParams messageParams = objectMapper.readValue(s, MessageParams.class);
+        assertThat(messageParams.toMap()).isEqualTo(params.toMap());
+    }
+}
