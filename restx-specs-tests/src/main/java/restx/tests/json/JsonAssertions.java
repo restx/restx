4529537e6c7154c0922dbd5c2674cc3f4da4cdef
xhanin <xavier.hanin@4sh.fr>
2014-02-04 22:48:52 +0100

    tests - introduce new json assertions

---
+++ restx-specs-tests/src/main/java/restx/tests/json/JsonAssertions.java
@@ -0,0 +1,148 @@
+package restx.tests.json;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.LinkedListMultimap;
+import com.google.common.collect.Multimap;
+
+import java.io.File;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import static restx.common.MoreStrings.indent;
+
+/**
+ * Date: 4/2/14
+ * Time: 21:34
+ */
+public class JsonAssertions {
+    public static JsonAssertions assertThat(String json) {
+        return new JsonAssertions(new StringJsonSource("actual", json));
+    }
+    public static JsonAssertions assertThat(File json, Charset cs) {
+        return new JsonAssertions(new FileJsonSource(json, cs));
+    }
+    public static JsonAssertions assertThat(URL json, Charset cs) {
+        return new JsonAssertions(new URLJsonSource(json, cs));
+    }
+    public static JsonAssertions assertThat(JsonSource json) {
+        return new JsonAssertions(json);
+    }
+
+    private final JsonDiffer differ;
+    private final JsonSource actual;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private JsonAssertions(JsonSource actual) {
+        this.actual = actual;
+        differ = new JsonDiffer();
+    }
+
+    public JsonAssertions allowingExtraUnexpectedFields() {
+        differ.getLeftConfig().setIgnoreExtraFields(true);
+        return this;
+    }
+
+    public JsonAssertions isSameJsonAs(String expected) {
+        return isSameJsonAs(new StringJsonSource("expected", expected));
+    }
+    public JsonAssertions isSameJsonAs(File expected, Charset cs) {
+        return isSameJsonAs(new FileJsonSource(expected, cs));
+    }
+    public JsonAssertions isSameJsonAs(URL expected, Charset cs) {
+        return isSameJsonAs(new URLJsonSource(expected, cs));
+    }
+    public JsonAssertions isSameJsonAs(JsonSource expected) {
+        JsonDiff diff = differ.compare(actual, expected);
+
+        if (!diff.isSame()) {
+            StringBuilder sb = new StringBuilder();
+
+            sb.append("Expecting:\n")
+                    .append(indent(actual.content(), 2)).append("\n")
+                    .append("to be same json as:\n")
+                    .append(indent(expected.content(), 2)).append("\n")
+                    .append("but following differences were found:\n");
+
+            Multimap<JsonObjectLocation, JsonDiff.Difference> differencesPerLocation = LinkedListMultimap.create();
+            for (JsonDiff.Difference difference : diff.getDifferences()) {
+                differencesPerLocation.put(difference.getLeftContext(), difference);
+            }
+
+            int i = 1;
+            for (JsonObjectLocation context : differencesPerLocation.keySet()) {
+                sb.append(String.format("- within [L%dC%d]-[L%dC%d]:\n",
+                        context.getFrom().getLineNr(), context.getFrom().getColumnNr(),
+                        context.getTo().getLineNr(), context.getTo().getColumnNr()))
+                        .append(indent(context.getJson(), 2))
+                        .append("\n");
+                for (JsonDiff.Difference difference : differencesPerLocation.get(context)) {
+                    if (difference instanceof JsonDiff.AddedKey) {
+                        JsonDiff.AddedKey addedKey = (JsonDiff.AddedKey) difference;
+                        sb.append(String.format("%02d) ", i))
+                                .append("missing key '").append(addedKey.getKey()).append("'")
+                                .append(" at path '").append(addedKey.getLeftPath()).append("'")
+                                .append(" expected value:\n")
+                                .append(indent(asJson(addedKey.getValue()), 6))
+                                .append("\n");;
+                    }
+                    if (difference instanceof JsonDiff.RemovedKey) {
+                        JsonDiff.RemovedKey removedKey = (JsonDiff.RemovedKey) difference;
+
+                        sb.append(String.format("%02d) ", i))
+                                .append("extra key '").append(removedKey.getKey()).append("'")
+                                .append(" at path '").append(removedKey.getLeftPath()).append("'")
+                                .append(" with value:\n")
+                                .append(indent(asJson(removedKey.getValue()), 6))
+                                .append("\n");;
+                    }
+                    if (difference instanceof JsonDiff.ValueDiff) {
+                        JsonDiff.ValueDiff valueDiff = (JsonDiff.ValueDiff) difference;
+
+                        sb.append(String.format("%02d) ", i))
+                                .append("expected value ").append(asJson(valueDiff.getRightValue()))
+                                .append(" but was ").append(asJson(valueDiff.getLeftValue()))
+                                .append(" at path '").append(valueDiff.getLeftPath()).append("'\n");
+                    }
+                    if (difference instanceof JsonDiff.ArrayInsertedValue) {
+                        JsonDiff.ArrayInsertedValue arrayInsertedValue = (JsonDiff.ArrayInsertedValue) difference;
+
+                        sb.append(String.format("%02d) ", i))
+                                .append("missing element(s) in array at position ").append(arrayInsertedValue.getLeftPosition())
+                                .append(" at path '").append(arrayInsertedValue.getLeftPath()).append("'")
+                                .append(" expected value(s):\n")
+                                .append(indent(asJson(arrayInsertedValue.getValues()), 6))
+                                .append("\n");
+                    }
+                    if (difference instanceof JsonDiff.ArrayDeletedValue) {
+                        JsonDiff.ArrayDeletedValue arrayDeletedValue = (JsonDiff.ArrayDeletedValue) difference;
+
+                        sb.append(String.format("%02d) ", i))
+                                .append("extra element(s) in array at position ").append(arrayDeletedValue.getLeftPosition())
+                                .append(" at path '").append(arrayDeletedValue.getLeftPath()).append("'")
+                                .append(" with value(s):\n")
+                                .append(indent(asJson(arrayDeletedValue.getValues()), 6))
+                                .append("\n");
+                    }
+                    i++;
+                }
+            }
+
+            throw new AssertionError(sb.toString());
+        }
+        return this;
+    }
+
+    private String asJson(Object o) {
+        if (o == null) {
+            return "null";
+        }
+
+        try {
+            return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(o);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return o.toString();
+        }
+    }
+}
---
+++ restx-specs-tests/src/test/java/restx/tests/json/JsonAssertionsTest.java
@@ -0,0 +1,127 @@
+package restx.tests.json;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+/**
+ * Date: 4/2/14
+ * Time: 21:45
+ */
+public class JsonAssertionsTest {
+    @Test
+    public void should_not_throw_exception_when_same() throws Exception {
+        JsonAssertions.assertThat("{}").isSameJsonAs("{}");
+    }
+
+    @Test
+    public void should_not_throw_exception_on_extra_field_when_allowed() throws Exception {
+        JsonAssertions.assertThat("{\"key1\":\"val1\"}")
+                .allowingExtraUnexpectedFields().isSameJsonAs("{}");
+    }
+
+    @Test
+    public void should_throw_exception_on_extra_field_when_not_allowed() throws Exception {
+        try {
+            JsonAssertions.assertThat("{\"key1\":\"val1\"}").isSameJsonAs("{}");
+            fail("should throw error when not same");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage(
+                    "Expecting:\n" +
+                            "  {\"key1\":\"val1\"}\n" +
+                            "to be same json as:\n" +
+                            "  {}\n" +
+                            "but following differences were found:\n" +
+                            "- within [L1C2]-[L1C16]:\n" +
+                            "  {\"key1\":\"val1\"}\n" +
+                            "01) extra key 'key1' at path '.' with value:\n" +
+                            "      \"val1\"\n"
+            );
+        }
+    }
+
+    @Test
+    public void should_throw_exception_when_not_same() throws Exception {
+        try {
+            JsonAssertions.assertThat("{}").isSameJsonAs("{\"key1\":\"val1\"}");
+            fail("should throw error when not same");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage(
+                    "Expecting:\n" +
+                            "  {}\n" +
+                            "to be same json as:\n" +
+                            "  {\"key1\":\"val1\"}\n" +
+                            "but following differences were found:\n" +
+                            "- within [L1C2]-[L1C3]:\n" +
+                            "  {}\n" +
+                            "01) missing key 'key1' at path '.' expected value:\n" +
+                            "      \"val1\"\n"
+            );
+        }
+    }
+
+    @Test
+    public void should_throw_exception_when_diff_value() throws Exception {
+        try {
+            JsonAssertions.assertThat("{\"key1\":\"val2\"}").isSameJsonAs("{\"key1\":\"val1\"}");
+            fail("should throw error when not same");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage(
+                    "Expecting:\n" +
+                            "  {\"key1\":\"val2\"}\n" +
+                            "to be same json as:\n" +
+                            "  {\"key1\":\"val1\"}\n" +
+                            "but following differences were found:\n" +
+                            "- within [L1C2]-[L1C16]:\n" +
+                            "  {\"key1\":\"val2\"}\n" +
+                            "01) expected value \"val1\" but was \"val2\" at path './key1'\n"
+            );
+        }
+    }
+
+    @Test
+    public void should_throw_exception_when_inserted_array_value() throws Exception {
+        try {
+            JsonAssertions.assertThat("{\"key1\": [{\"key2\": \"val2\"}]}").isSameJsonAs("{\"key1\": []}");
+            fail("should throw error when not same");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage(
+                    "Expecting:\n" +
+                            "  {\"key1\": [{\"key2\": \"val2\"}]}\n" +
+                            "to be same json as:\n" +
+                            "  {\"key1\": []}\n" +
+                            "but following differences were found:\n" +
+                            "- within [L1C2]-[L1C29]:\n" +
+                            "  {\"key1\": [{\"key2\": \"val2\"}]}\n" +
+                            "01) extra element(s) in array at position 0 at path './key1' with value(s):\n" +
+                            "      [ {\n" +
+                            "        \"key2\" : \"val2\"\n" +
+                            "      } ]\n"
+            );
+        }
+    }
+
+    @Test
+    public void should_throw_exception_when_missing_value() throws Exception {
+        try {
+            JsonAssertions.assertThat("{\"key1\": []}").isSameJsonAs("{\"key1\": [{\"key2\": \"val2\"}]}");
+            fail("should throw error when not same");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage(
+                    "Expecting:\n" +
+                            "  {\"key1\": []}\n" +
+                            "to be same json as:\n" +
+                            "  {\"key1\": [{\"key2\": \"val2\"}]}\n" +
+                            "but following differences were found:\n" +
+                            "- within [L1C2]-[L1C13]:\n" +
+                            "  {\"key1\": []}\n" +
+                            "01) missing element(s) in array at position 0 at path './key1' expected value(s):\n" +
+                            "      [ {\n" +
+                            "        \"key2\" : \"val2\"\n" +
+                            "      } ]\n"
+            );
+        }
+    }
+
+}
