7d7b0760fe9225265948f9171d523088562805da
apeyrard <augustin.peyrard@gmail.com>
2014-03-02 18:21:44 +0100

    - Correct the bug with browser autofill (This bug is due to a known bug of angularjs https://github.com/angular/angular.js/issues/1460, but as the correction is a little bit too much code to add, I prefer a less verbose solution found there: http://victorblog.com/2014/01/12/fixing-autocomplete-autofill-on-angularjs-form-submit/). - Default focus on the username input (using the html5 attribute autofocus). - Change login button type to "submit" in order to be able to validate the form with the enter key.

--- restx-admin/src/main/resources/restx/admin/js/login.js
+++ restx-admin/src/main/resources/restx/admin/js/login.js
@@ -5,8 +5,8 @@ angular.module('admin').factory('baseUri', function() {
 });
 
 angular.module('admin').controller('LoginController', function($scope, baseUri, $http) {
-    $scope.authenticate = function(username, password) {
-        $http.post(baseUri + '/sessions', {principal: {name: username, passwordHash: SparkMD5.hash(password)}})
+    $scope.authenticate = function() {
+        $http.post(baseUri + '/sessions', {principal: {name: $scope.username, passwordHash: SparkMD5.hash($scope.password)}})
             .success(function(data, status, headers, config) {
                 console.log('authenticated', data, status);
                 window.location = $.querystring('backTo') || (baseUri + '/@/ui/');
@@ -15,4 +15,19 @@ angular.module('admin').controller('LoginController', function($scope, baseUri, 
                 alertify.success("Authentication error, please try again.");
             });;
     }
+});
+
+angular.module('admin').directive('formAutofillFix', function($timeout) {
+    return function(scope, elem, attrs) {
+        // Fix autofill issues where Angular doesn't know about autofilled inputs
+        if(attrs.ngSubmit) {
+            $timeout(function() {
+                elem.unbind('submit').submit(function(e) {
+                    e.preventDefault();
+                    elem.find('input, textarea, select').trigger('input').trigger('change').trigger('keydown');
+                    scope.$apply(attrs.ngSubmit);
+                });
+            });
+        }
+    };
 });
