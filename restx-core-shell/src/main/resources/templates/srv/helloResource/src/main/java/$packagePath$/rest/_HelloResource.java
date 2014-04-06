78bbfb9c5f704240114eb20449e3f6ea3f261427
xhanin <xavier.hanin@4sh.fr>
2014-04-06 14:29:00 +0200

    [shell] add unsecured resource in generated hello resource
    Makes it easier to test the REST API

--- restx-core-shell/src/main/resources/templates/srv/helloResource/src/main/java/$packagePath$/rest/_HelloResource.java
+++ restx-core-shell/src/main/resources/templates/srv/helloResource/src/main/java/$packagePath$/rest/_HelloResource.java
@@ -6,13 +6,39 @@ import org.joda.time.DateTime;
 import restx.annotations.GET;
 import restx.annotations.RestxResource;
 import restx.factory.Component;
+import restx.security.PermitAll;
 import restx.security.RolesAllowed;
+import restx.security.RestxSession;
 
 @Component @RestxResource
 public class HelloResource {
+
+    /**
+     * Say hello to currently logged in user.
+     *
+     * Authorized only for principals with Roles.HELLO_ROLE role.
+     *
+     * @return a Message to say hello
+     */
     @GET("/message")
     @RolesAllowed(Roles.HELLO_ROLE)
-    public Message sayHello(String who) {
+    public Message sayHello() {
+        return new Message().setMessage(String.format(
+                "hello %s, it's %s",
+                RestxSession.current().getPrincipal().get().getName(),
+                DateTime.now().toString("HH:mm:ss")));
+    }
+
+    /**
+     * Say hello to anybody.
+     *
+     * Does not require authentication.
+     *
+     * @return a Message to say hello
+     */
+    @GET("/hello")
+    @PermitAll
+    public Message helloPublic(String who) {
         return new Message().setMessage(String.format(
                 "hello %s, it's %s",
                 who, DateTime.now().toString("HH:mm:ss")));
