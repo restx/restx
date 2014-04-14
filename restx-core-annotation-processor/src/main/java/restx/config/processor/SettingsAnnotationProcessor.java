5499113ace7e1bb746196da2e4ed0ddef39a2a55
Thomas Recloux <thomas.recloux@gmail.com>
2014-04-14 07:21:02 +0200

    Add java.util.Optional support for Settings

--- restx-core-annotation-processor/src/main/java/restx/config/processor/SettingsAnnotationProcessor.java
+++ restx-core-annotation-processor/src/main/java/restx/config/processor/SettingsAnnotationProcessor.java
@@ -1,28 +1,27 @@
 package restx.config.processor;
 
-import com.google.common.base.*;
+import com.google.common.base.Optional;
+import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Lists;
 import com.samskivert.mustache.Template;
 import restx.common.Mustaches;
 import restx.common.processor.RestxAbstractProcessor;
 import restx.config.Settings;
 import restx.config.SettingsKey;
-import restx.exceptions.ErrorCode;
-import restx.exceptions.ErrorField;
 
-import javax.annotation.processing.*;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedOptions;
+import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
-import javax.lang.model.element.*;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeMirror;
-import javax.tools.Diagnostic;
-import javax.tools.JavaFileObject;
-import java.io.IOException;
-import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.Set;
 
 /**
@@ -69,10 +68,12 @@ public class SettingsAnnotationProcessor extends RestxAbstractProcessor {
                         }
 
                         String accessorReturnType = methodElem.getReturnType().toString();
-                        boolean optional;
                         String targetType;
-                        if (accessorReturnType.startsWith(Optional.class.getCanonicalName())) {
-                            optional = true;
+                        String prefix;
+                        String suffix;
+                        boolean guavaOptional = accessorReturnType.startsWith(Optional.class.getCanonicalName());
+                        boolean java8Optional = accessorReturnType.startsWith("java.util.Optional");
+                        if (guavaOptional || java8Optional) {
                             List<? extends TypeMirror> typeArguments = ((DeclaredType) methodElem.getReturnType()).getTypeArguments();
                             if (typeArguments.isEmpty()) {
                                 error(String.format("unsupported return type %s for settings accessor method" +
@@ -83,8 +84,17 @@ public class SettingsAnnotationProcessor extends RestxAbstractProcessor {
                                 targetType = typeArguments.get(0).toString();
                             }
                         } else {
-                            optional = false;
                             targetType = accessorReturnType;
+                        }
+                        if (guavaOptional) {
+                            prefix = "";
+                            suffix = "";
+                        } else if (java8Optional) {
+                            prefix = "java.util.Optional.ofNullable(";
+                            suffix = ".orNull())";
+                        } else {
+                            prefix = "";
+                            suffix = ".get()";
                         }
                         String configAccessor;
                         switch (targetType) {
@@ -118,7 +128,8 @@ public class SettingsAnnotationProcessor extends RestxAbstractProcessor {
                                 .put("configAccessor", configAccessor)
                                 .put("accessorName", methodElem.getSimpleName().toString())
                                 .put("key", settingsKey.key())
-                                .put("get", optional ? "" : ".get()")
+                                .put("prefix", prefix)
+                                .put("suffix", suffix)
                                 .put("doc", settingsKey.doc())
                                 .put("defaultValue", settingsKey.defaultValue())
                                 .build());
