4a3b34ef4158575c51f1dc3a8ec91b7d1533a52d
Thomas Recloux <thomas.recloux@gmail.com>
2014-04-16 16:34:24 +0200

    The missing file

--- restx-samplest-java8/src/main/java/samplest/settings/MySettings.java
+++ restx-samplest-java8/src/main/java/samplest/settings/MySettings.java
@@ -1,20 +1,14 @@
 package samplest.settings;
 
-import com.google.common.base.Optional;
 import restx.config.Settings;
 import restx.config.SettingsKey;
 
-/**
- * Date: 11/11/13
- * Time: 17:33
- */
+import java.util.Optional;
+
 @Settings
 public interface MySettings {
     @SettingsKey(key = "example.key1")
     Optional<String> key1();
-    @SettingsKey(key = "example.key2", defaultValue = "MyValue2")
-    String key2();
-    @SettingsKey(key = "example.key3", defaultValue = "MyValue3",
-            doc = "This is an example key 3")
-    String key3();
+    @SettingsKey(key = "example.key2", defaultValue = "key2")
+    Optional<String> key2();
 }
