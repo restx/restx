3953f075b923bde65b681e676998b3d51249838b
fcamblor <frederic.camblor@4sh.fr>
2014-04-20 17:36:39 +0200

    Allowing to configure router path when using @RunWith(RestxSpecTestsRunner.class) and @FindSpecsIn()

--- restx-specs-tests/src/main/java/restx/tests/FindSpecsIn.java
+++ restx-specs-tests/src/main/java/restx/tests/FindSpecsIn.java
@@ -9,4 +9,5 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface FindSpecsIn {
     String value();
+    String withRouterPath() default "/api";
 }
