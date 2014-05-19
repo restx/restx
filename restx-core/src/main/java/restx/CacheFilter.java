d91fcfd41ad5859ae7326396d20ba1daaecda808
Xavier Hanin <xavier.hanin@gmail.com>
2014-05-19 19:09:19 +0200

    core - allow to set cache-control header specifically
    without needing to disable cache filter for the path

--- restx-core/src/main/java/restx/CacheFilter.java
+++ restx-core/src/main/java/restx/CacheFilter.java
@@ -8,10 +8,21 @@ import restx.factory.Component;
 import java.io.IOException;
 
 /**
- * Date: 1/12/13
- * Time: 14:55
+ * This filter is used to set a Cache-Control no-cache header on responses.
+ *
+ * This disable caching by default on resources, the content of them being most of the time dynamic, it's
+ * easier to have cache disabled by default.
+ *
+ * This can be configured in the settings with the cachedResources, which allow to set the restx paths on which this
+ * filter should not be applied.
+ *
+ * Moreover, this filter won't set the Cache-Control header if it's already set, so it doesn't prevent other more
+ * specific caching strategies.
+ *
+ * Finally, it's also possible to disable this filter as any other Restx component, by setting
+ * restx.activation::restx.GzipFilter::CacheFilter=false
  */
-@Component(priority = 1000)
+@Component(priority = -100)
 public class CacheFilter implements RestxFilter {
     private final ImmutableList<String> paths;
 
@@ -40,7 +51,9 @@ public class CacheFilter implements RestxFilter {
                         ctx.nextHandlerMatch().handle(req, resp, ctx.withListener(new AbstractRouteLifecycleListener() {
                             @Override
                             public void onBeforeWriteContent(RestxRequest req, RestxResponse resp) {
-                                resp.setHeader("Cache-Control", "no-cache");
+                                if (!resp.getHeader("Cache-Control").isPresent()) {
+                                    resp.setHeader("Cache-Control", "no-cache");
+                                }
                             }
                         }));
                     }
