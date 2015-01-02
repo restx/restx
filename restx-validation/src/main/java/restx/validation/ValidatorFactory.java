333ea6a1164eed43be6192fbbb72f6aa1cf81b21
fcamblor <frederic.camblor@4sh.fr>
2015-01-02 12:24:28 +0100

    validation - provided named component to ignore the ignoreXmlConfiguration() call if needed

--- restx-validation/src/main/java/restx/validation/ValidatorFactory.java
+++ restx-validation/src/main/java/restx/validation/ValidatorFactory.java
@@ -1,6 +1,7 @@
 package restx.validation;
 
 import org.hibernate.validator.HibernateValidator;
+import org.hibernate.validator.HibernateValidatorConfiguration;
 import restx.factory.Module;
 import restx.factory.Name;
 import restx.factory.Provides;
@@ -17,11 +18,21 @@ import javax.validation.Validator;
 @Module
 public class ValidatorFactory {
     public static final String VALIDATOR_NAME = "hibernate.validator";
+    public static final String IGNORE_XML_CONFIGURATION_NAME = "hibernate.validator.ignore.xml.configuration";
     public static final Name<Validator> VALIDATOR = Name.of(Validator.class, VALIDATOR_NAME);
 
     @Provides @Named(VALIDATOR_NAME)
-    public Validator validator() {
-        return Validation.byProvider(HibernateValidator.class).configure()
-                        .ignoreXmlConfiguration().buildValidatorFactory().getValidator();
+    public Validator validator(@Named(IGNORE_XML_CONFIGURATION_NAME) Boolean ignoreXmlConfiguration) {
+        HibernateValidatorConfiguration config = Validation.byProvider(HibernateValidator.class).configure();
+        if(ignoreXmlConfiguration) {
+            config.ignoreXmlConfiguration();
+        }
+        return config.buildValidatorFactory().getValidator();
+    }
+
+    // Perf improvement to greatly fasten startup time
+    @Provides @Named(IGNORE_XML_CONFIGURATION_NAME)
+    public Boolean ignoreXmlConfigurationFlag(){
+        return Boolean.TRUE;
     }
 }
