bf827e7c2669f4bd0ca817462107d9a8e8b53980
fcamblor <frederic.camblor@4sh.fr>
2015-01-23 18:45:10 +0100

    webjars - Caching every webjars for 100 days

--- restx-webjars/src/main/java/restx/webjars/WebJarsModule.java
+++ restx-webjars/src/main/java/restx/webjars/WebJarsModule.java
@@ -1,8 +1,13 @@
 package restx.webjars;
 
+import com.google.common.base.Predicates;
+import com.google.common.collect.ImmutableMap;
 import restx.ResourcesRoute;
 import restx.factory.Module;
 import restx.factory.Provides;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author fcamblor
@@ -12,6 +17,15 @@ public class WebJarsModule {
 
     @Provides
     public ResourcesRoute webjars() {
-        return new ResourcesRoute("WebjarsRoute", "/@/webjars", "META-INF/resources/webjars");
+        return new ResourcesRoute(
+                "WebjarsRoute", "/@/webjars", "META-INF/resources/webjars",
+                ImmutableMap.<String, String>of(),
+                Arrays.asList(
+                        new ResourcesRoute.CachedResourcePolicy(
+                                Predicates.<ResourcesRoute.ResourceInfo>alwaysTrue(),
+                                "max-age="+ TimeUnit.DAYS.toSeconds(100)
+                        )
+                )
+        );
     }
 }
