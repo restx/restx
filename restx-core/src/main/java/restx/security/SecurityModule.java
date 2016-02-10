11efb54c9e407684245f055b7cbaf727c87648bd
Augustin Peyrard <augustin.peyrard@gmail.com>
2016-02-10 09:15:18 +0100

    provides a default route to handle 404 response in prod mode
    The factory was listing all the existing endpoints if no route was
    found for a request. By defining this new route, which match all
    kind of request, we will bypass the `RestxMainRouterFactory`.
    This route is only activated in "prod" mode, as the listing might
    be useful during development.

--- restx-core/src/main/java/restx/security/SecurityModule.java
+++ restx-core/src/main/java/restx/security/SecurityModule.java
@@ -1,13 +1,27 @@
 package restx.security;
 
+import com.google.common.base.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import restx.RestxContext;
+import restx.RestxHandler;
+import restx.RestxHandlerMatch;
+import restx.RestxRequest;
+import restx.RestxRequestMatch;
+import restx.RestxResponse;
+import restx.RestxRoute;
+import restx.StdRestxRequestMatch;
+import restx.WebException;
 import restx.common.RestxConfig;
 import restx.config.Settings;
 import restx.config.SettingsKey;
 import restx.factory.AutoStartable;
 import restx.factory.Module;
 import restx.factory.Provides;
+import restx.factory.When;
+import restx.http.HttpStatus;
 
 import javax.inject.Named;
 
@@ -56,6 +70,25 @@ public class SecurityModule {
             @Override
             public void start() {
                 logger.debug("starting sessions statistics");
+            }
+        };
+    }
+
+    @Provides(priority = 100000)
+    @When(name = "restx.mode", value = "prod")
+    public RestxRoute productionNotFoundHandler() {
+        return new RestxRoute() {
+            @Override
+            public Optional<RestxHandlerMatch> match(RestxRequest req) {
+                return Optional.of(new RestxHandlerMatch(
+                        new StdRestxRequestMatch("*", req.getRestxPath()),
+                        new RestxHandler() {
+                            @Override
+                            public void handle(RestxRequestMatch match, RestxRequest req, RestxResponse resp, RestxContext ctx) throws IOException {
+                                throw new WebException(HttpStatus.NOT_FOUND);
+                            }
+                        }
+                ));
             }
         };
     }
