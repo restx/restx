248a30f169103b3a444f8ee93615de98233a212b
fcamblor <frederic.camblor@4sh.fr>
2016-05-19 16:27:35 +0200

    query-params - Added samplest for query parameters deserialization

--- restx-samplest/src/main/java/samplest/core/ParametersResource.java
+++ restx-samplest/src/main/java/samplest/core/ParametersResource.java
@@ -1,6 +1,10 @@
 package samplest.core;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.ObjectArrays;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import restx.annotations.GET;
@@ -8,6 +12,12 @@ import restx.annotations.POST;
 import restx.annotations.RestxResource;
 import restx.factory.Component;
 import restx.security.PermitAll;
+
+import javax.validation.constraints.Size;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Date: 13/12/13
@@ -41,12 +51,12 @@ public class ParametersResource {
         return "a=" + a + " b=" + b + " c=" + c + " d=" + d;
     }
 
-    @GET("/params/query/1")
+    @GET("/params/query/withOptionalString")
     public String queryparams(String a, Optional<String> b) {
         return "a=" + a + " b=" + b.or("default");
     }
 
-    @GET("/params/query/2")
+    @GET("/params/query/withOptionalDate")
     public String queryparams2(DateTime a, Optional<DateTime> b) {
         return "a=" + a + " b=" + b.or(new DateTime(0L, DateTimeZone.UTC));
     }
@@ -59,5 +69,65 @@ public class ParametersResource {
     @POST("/params/mandatoryPost")
     public POJO postContent(POJO pojo) {
         return pojo;
+    }
+
+    @GET("/params/listStringParams")
+    public List<String> listStringParams(List<String> params, List<String> otherParams) {
+        return FluentIterable.from(Iterables.concat(params, otherParams)).toList();
+    }
+
+    @GET("/params/setStringParams")
+    public Set<String> setStringParams(Set<String> params, Set<String> otherParams) {
+        return FluentIterable.from(Iterables.concat(params, otherParams)).toSet();
+    }
+
+    @GET("/params/iterableStringParams")
+    public Iterable<String> iterableStringParams(Iterable<String> params, Iterable<String> otherParams) {
+        return Iterables.concat(params, otherParams);
+    }
+
+    @GET("/params/optionalIterableStringParams")
+    public Iterable<String> optionalIterableStringParams(Optional<Iterable<String>> params, Optional<Iterable<String>> otherParams) {
+        return Iterables.concat(params.or(Collections.<String>emptyList()), otherParams.or(Collections.<String>emptyList()));
+    }
+
+    @GET("/params/arrayedStringParams")
+    public String[] arrayedStringParams(String[] params, String[] otherParams) {
+        return ObjectArrays.concat(params, otherParams, String.class);
+    }
+
+    @GET("/params/optionalArrayedStringParams")
+    public String[] optionalArrayedStringParams(Optional<String[]> params, Optional<String[]> otherParams) {
+        return ObjectArrays.concat(params.or(new String[0]), otherParams.or(new String[0]), String.class);
+    }
+
+    @GET("/params/listJodaDatesParams")
+    public List<DateTime> listJodaDatesParams(List<DateTime> params, List<DateTime> otherParams) {
+        return FluentIterable.from(Iterables.concat(params, otherParams)).toList();
+    }
+
+    @GET("/params/setJodaDatesParams")
+    public Set<DateTime> setJodaDatesParams(Set<DateTime> params, Set<DateTime> otherParams) {
+        return FluentIterable.from(Iterables.concat(params, otherParams)).toSet();
+    }
+
+    @GET("/params/iterableJodaDatesParams")
+    public Iterable<DateTime> iterableJodaDatesParams(Iterable<DateTime> params, Iterable<DateTime> otherParams) {
+        return Iterables.concat(params, otherParams);
+    }
+
+    @GET("/params/optionalIterableJodaDatesParams")
+    public Iterable<DateTime> optionalIterableJodaDatesParams(Optional<Iterable<DateTime>> params, Optional<Iterable<DateTime>> otherParams) {
+        return Iterables.concat(params.or(Collections.<DateTime>emptyList()), otherParams.or(Collections.<DateTime>emptyList()));
+    }
+
+    @GET("/params/arrayedJodaDatesParams")
+    public DateTime[] arrayedJodaDatesParams(DateTime[] params, DateTime[] otherParams) {
+        return ObjectArrays.concat(params, otherParams, DateTime.class);
+    }
+
+    @GET("/params/optionalArrayedJodaDatesParams")
+    public DateTime[] optionalArrayedJodaDatesParams(Optional<DateTime[]> params, Optional<DateTime[]> otherParams) {
+        return ObjectArrays.concat(params.or(new DateTime[0]), otherParams.or(new DateTime[0]), DateTime.class);
     }
 }
--- restx-samplest/src/test/java/samplest/core/ParametersResourceTest.java
+++ restx-samplest/src/test/java/samplest/core/ParametersResourceTest.java
@@ -24,40 +24,4 @@ public class ParametersResourceTest {
         assertThat(httpRequest.code()).isEqualTo(200);
         assertThat(httpRequest.body().trim()).isEqualTo("a=v1 b=v2 c=35 d=v4");
     }
-
-    @Test
-    public void should_return_query_params() throws Exception {
-        HttpRequest httpRequest = server.client().authenticatedAs("admin").GET(
-                "/api/params/query/1?a=v1&b=v2");
-        assertThat(httpRequest.code()).isEqualTo(200);
-        assertThat(httpRequest.body().trim()).isEqualTo("a=v1 b=v2");
-    }
-
-    @Test
-    public void should_return_query_params_without_optional() throws Exception {
-        HttpRequest httpRequest = server.client().authenticatedAs("admin").GET(
-                "/api/params/query/1?a=v1");
-        assertThat(httpRequest.code()).isEqualTo(200);
-        assertThat(httpRequest.body().trim()).isEqualTo("a=v1 b=default");
-    }
-
-    @Test
-    public void should_return_query_params_datetime() throws Exception {
-        HttpRequest httpRequest = server.client().authenticatedAs("admin").GET(
-                "/api/params/query/2" +
-                        "?a=" + URLEncoder.encode("2013-11-20T14:00:00.000+01:00", "UTF-8") +
-                        "&b=" + URLEncoder.encode("2013-12-20T14:00:00.000+01:00", "UTF-8"));
-        assertThat(httpRequest.code()).isEqualTo(200);
-        assertThat(httpRequest.body().trim()).isEqualTo("a=2013-11-20T13:00:00.000Z b=2013-12-20T13:00:00.000Z");
-    }
-
-    @Test
-    public void should_return_query_params_datetime_without_optional() throws Exception {
-        HttpRequest httpRequest = server.client().authenticatedAs("admin").GET(
-                "/api/params/query/2" +
-                        "?a=" + URLEncoder.encode("2013-11-20T14:00:00.000+01:00", "UTF-8"));
-        assertThat(httpRequest.code()).isEqualTo(200);
-        assertThat(httpRequest.body().trim()).isEqualTo("a=2013-11-20T13:00:00.000Z b=1970-01-01T00:00:00.000Z");
-    }
-
 }
