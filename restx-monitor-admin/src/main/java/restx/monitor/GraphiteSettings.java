3db7d18ac42cb681551285a6b5603ad26cae627f
fcamblor <fcamblor@gmail.com>
2017-08-25 00:29:38 +0200

    [breaking] metrics - prefixed every graphite settings with 'restx.' to avoid collision with pre-existing properties defined in the app server. This fixes #135

--- restx-monitor-admin/src/main/java/restx/monitor/GraphiteSettings.java
+++ restx-monitor-admin/src/main/java/restx/monitor/GraphiteSettings.java
@@ -10,17 +10,17 @@ import restx.config.SettingsKey;
  */
 @Settings
 public interface GraphiteSettings {
-    @SettingsKey(key = "graphite.host", doc = "the host at which a graphite server is listening to collect metrics info" +
+    @SettingsKey(key = "restx.graphite.host", doc = "the host at which a graphite server is listening to collect metrics info" +
             " - set it to activate graphite reporting")
     Optional<String> getGraphiteHost();
-    @SettingsKey(key = "graphite.port", doc = "the port at which a graphite server is listening to collect metrics info")
+    @SettingsKey(key = "restx.graphite.port", doc = "the port at which a graphite server is listening to collect metrics info")
     Optional<Integer> getGraphitePort();
 
-    @SettingsKey(key = "graphite.reporter.frequency", doc = "the frequency at which data is sent to graphite", defaultValue = "1")
+    @SettingsKey(key = "restx.graphite.reporter.frequency", doc = "the frequency at which data is sent to graphite", defaultValue = "1")
     Optional<Integer> getFrequency();
-    @SettingsKey(key = "graphite.reporter.frequency.unit", doc = "the unit in which frequency is expressed", defaultValue = "MINUTES")
+    @SettingsKey(key = "restx.graphite.reporter.frequency.unit", doc = "the unit in which frequency is expressed", defaultValue = "MINUTES")
     Optional<String> getFrequencyUnit();
 
-    @SettingsKey(key = "graphite.prefix", doc = "the value prepended to all metrics names which are sent to graphite")
+    @SettingsKey(key = "restx.graphite.prefix", doc = "the value prepended to all metrics names which are sent to graphite")
     Optional<String> getPrefix();
 }
