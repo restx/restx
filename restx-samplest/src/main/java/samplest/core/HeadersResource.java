fbdeeb26905fe9b2490251c05ee8bd5a6a5a3891
fcamblor <fcamblor@gmail.com>
2017-09-05 23:48:44 +0200

    headers - introduced @LocationHeader annotation allowing to send Location header with an URL interpolating object fields returned by an endpoint. Implemented this behaviour through new LocationHeaderFilter.

--- restx-samplest/src/main/java/samplest/core/HeadersResource.java
+++ restx-samplest/src/main/java/samplest/core/HeadersResource.java
@@ -1,8 +1,6 @@
 package samplest.core;
 
-import restx.annotations.ExpiresAfter;
-import restx.annotations.GET;
-import restx.annotations.RestxResource;
+import restx.annotations.*;
 import restx.factory.Component;
 
 @RestxResource("/headers") @Component
@@ -14,4 +12,40 @@ public class HeadersResource {
         return "hello";
     }
 
+    public static class Foo {
+        String id;
+        String name;
+
+        public String getId() {
+            return id;
+        }
+
+        Foo setId(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Foo setName(String name) {
+            this.name = name;
+            return this;
+        }
+    }
+
+    @POST("/foos")
+    @LocationHeader("{_currentUri_}/{id}")
+    public Foo locationHeader(Foo foo) {
+        foo.setId("123456");
+        return foo;
+    }
+
+    @POST("/foos2")
+    @LocationHeader("{_baseUri_}/headers/foos2/{id}")
+    public Foo locationHeader2(Foo foo) {
+        foo.setId("123456");
+        return foo;
+    }
 }
--- restx-samplest/src/test/java/samplest/core/HeadersResourceTest.java
+++ restx-samplest/src/test/java/samplest/core/HeadersResourceTest.java
@@ -21,4 +21,24 @@ public class HeadersResourceTest {
         long duration = Math.abs(httpRequest.expires() - DateTime.now().plusDays(2).plusHours(4).getMillis());
         assertThat(duration).isLessThan(5000);
     }
+
+    @Test
+    public void should_return_location_header_with_current_uri() throws Exception {
+        HttpRequest httpRequest = server.client().authenticatedAs("admin")
+                .POST("/api/headers/foos")
+                .send("{\"name\":\"FOO\"}");
+
+        assertThat(httpRequest.code()).isEqualTo(200);
+        assertThat(httpRequest.location()).isEqualTo(server.getServer().baseUrl()+"/api/headers/foos/123456");
+    }
+
+    @Test
+    public void should_return_location_header_with_base_uri() throws Exception {
+        HttpRequest httpRequest = server.client().authenticatedAs("admin")
+                .POST("/api/headers/foos2")
+                .send("{\"name\":\"FOO\"}");
+
+        assertThat(httpRequest.code()).isEqualTo(200);
+        assertThat(httpRequest.location()).isEqualTo(server.getServer().baseUrl()+"/api/headers/foos2/123456");
+    }
 }
