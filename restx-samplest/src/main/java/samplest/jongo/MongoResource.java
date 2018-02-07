2469d34c9104b7c7ff322ed97b6e56f66ff97630
fcamblor <fcamblor@gmail.com>
2018-02-08 00:00:42 +0100

    Introduced tests on Jongo id generation, spotting regressions in 0.35 compared to 0.34 regarding id generation cases

---
+++ restx-samplest/src/main/java/samplest/jongo/MongoResource.java
@@ -0,0 +1,87 @@
+package samplest.jongo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.bson.types.ObjectId;
+import org.jongo.marshall.jackson.oid.Id;
+import org.jongo.marshall.jackson.oid.MongoId;
+import org.jongo.marshall.jackson.oid.MongoObjectId;
+import restx.annotations.POST;
+import restx.annotations.RestxResource;
+import restx.factory.Component;
+import restx.jongo.JongoCollection;
+import restx.security.PermitAll;
+
+import javax.inject.Named;
+
+@RestxResource @Component
+public class MongoResource {
+    public static class ObjectWithIdAnnotation {
+        // @Id // 0.34 -> OK ; 0.35 -> KO!
+        @MongoId @JsonProperty("_id") // 0.35 -> KO!
+        private String id;
+        private String label;
+
+        public String getId() { return id; }
+        public void setId(String id) { this.id = id; }
+        public String getLabel() { return label; }
+        public void setLabel(String label) { this.label = label; }
+    }
+    public static class ObjectWithObjectIdAnnotation {
+        // @org.jongo.marshall.jackson.oid.ObjectId @Id // 0.34 -> OK ; 0.35 -> KO!
+        @MongoId @MongoObjectId @JsonProperty("_id") // 0.35 -> KO!
+        private String id;
+        private String label;
+
+        public String getId() { return id; }
+        public void setId(String id) { this.id = id; }
+        public String getLabel() { return label; }
+        public void setLabel(String label) { this.label = label; }
+    }
+    public static class ObjectWithObjectIdType {
+        // @Id // 0.34 -> OK ; 0.35 -> OK
+        @MongoId @JsonProperty("_id") // 0.35 -> OK
+        private ObjectId id;
+        private String label;
+
+        public String getId() { return id.toString(); }
+        public void setId(ObjectId id) { this.id = id; }
+        public String getLabel() { return label; }
+        public void setLabel(String label) { this.label = label; }
+    }
+
+    private final JongoCollection objectWithIdAnnotationCollection;
+    private final JongoCollection objectWithObjectIdAnnotationCollection;
+    private final JongoCollection objectWithObjectIdTypeCollection;
+
+    public MongoResource(
+            @Named("objectWithIdAnnotationCollection") JongoCollection objectWithIdAnnotationCollection,
+            @Named("objectWithObjectIdAnnotationCollection") JongoCollection objectWithObjectIdAnnotationCollection,
+            @Named("objectWithObjectIdTypeCollection") JongoCollection objectWithObjectIdTypeCollection) {
+        this.objectWithIdAnnotationCollection = objectWithIdAnnotationCollection;
+        this.objectWithObjectIdAnnotationCollection = objectWithObjectIdAnnotationCollection;
+        this.objectWithObjectIdTypeCollection = objectWithObjectIdTypeCollection;
+    }
+
+    @POST("/mongo/objectsWithIdAnnotation")
+    @PermitAll
+    public ObjectWithIdAnnotation createFoo(ObjectWithIdAnnotation foo) {
+        this.objectWithIdAnnotationCollection.get().insert(foo);
+        return foo;
+    }
+
+    @POST("/mongo/objectsWithObjectIdAnnotation")
+    @PermitAll
+    public ObjectWithObjectIdAnnotation createObjectWithObjectIdAnnotation(ObjectWithObjectIdAnnotation objectWithObjectIdAnnotation) {
+        this.objectWithObjectIdAnnotationCollection.get().insert(objectWithObjectIdAnnotation);
+        return objectWithObjectIdAnnotation;
+    }
+
+    @POST("/mongo/objectsWithObjectIdType")
+    @PermitAll
+    public ObjectWithObjectIdType createObjectWithObjectId(ObjectWithObjectIdType objectWithObjectIdType) {
+        this.objectWithObjectIdTypeCollection.get().insert(objectWithObjectIdType);
+        return objectWithObjectIdType;
+    }
+
+
+}
---
+++ restx-samplest/src/test/java/samplest/jongo/MongoResourceSpecsTest.java
@@ -0,0 +1,10 @@
+package samplest.jongo;
+
+import org.junit.runner.RunWith;
+import restx.tests.FindSpecsIn;
+import restx.jongo.specs.tests.MongoRestxSpecTestsRunner;
+
+@RunWith(MongoRestxSpecTestsRunner.class)
+@FindSpecsIn("specs/mongo")
+public class MongoResourceSpecsTest {
+}
